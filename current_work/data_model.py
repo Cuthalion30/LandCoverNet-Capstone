3e58f0d9c6173ec1ae171cdd306f69d3f998a910
Cuthalion30 <90283634+Cuthalion30@users.noreply.github.com>
2023-03-12 15:14:29 -0400

    Start of the data model

--- current_work/data_model.py
+++ current_work/data_model.py
@@ -1,5 +1,18 @@
 import numpy as np
 import pandas as pd
+from lazypredict.Supervised import LazyClassifier
+from sklearn.model_selection import train_test_split
 
+# Read in the data
 df = pd.read_csv('landcovernet_africa_data.csv')
-print(df.describe())
+
+# separate the data from the labels
+# f1 (costal) and f2 (blue) are bands for landsat-8
+X, Y = df[['f1', 'f2']], df.labels
+
+# 80/20 split for the train/test
+X_train, X_test, y_train, y_test = train_test_split(X, Y, test_size=.2, random_state=1234)
+
+# fit all models
+clf = LazyClassifier(predictions=True)
+#models, predictions = clf.fit(X_train, X_test, y_train, y_test)
